262792b10ed363cb49c87ce06bf8d88564fc69da
SolInvictusx <ambrosi52@live.com>
2023-04-26 01:01:39 -0500

    Second Commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let js = 'amazing';
+console.log(40 + 9 + 23 - 10);
+
+console.log('Jonas');
+console.log(23);
+
+let firstName = 'Matilda';
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let $funciton = 27;
+
+let Person = 'Jonas';
+
+let myFirstJob = 'Trim crew member';
+let myCurrentJob = 'Teacher';
+
+console.log(myFirstJob);
