a705acfc90df7d689814a72a547fa99e7e5e7d0f
SolInvictusx <ambrosi52@live.com>
2023-05-20 16:43:49 -0500

    Strings and Template literals

--- script.js
+++ script.js
@@ -51,30 +51,70 @@ console.log(myFirstJob);
 // lastName = 'Smith';
 // console.log(lastName);
 
-let currentYear = 2023;
-const ageJonas = currentYear - 1991;
-const ageRyan = currentYear - 1995;
-console.log(ageJonas, ageRyan);
+// let currentYear = 2023;
+// const ageJonas = currentYear - 1991;
+// const ageRyan = currentYear - 1995;
+// console.log(ageJonas, ageRyan);
 
-console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
-// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+// console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// // 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
 
-const firstName = 'Jonas';
-const lastName = 'Smith';
+// const firstName = 'Jonas';
+// const lastName = 'Smith';
 
-console.log(firstName + ' ' + lastName);
+// console.log(firstName + ' ' + lastName);
 
-// Assignment operators
-let x = 10 + 5;
-x += 10;
-// This x = x + 10;
-x *= 4;
-x++;
-console.log(x);
+// // Assignment operators
+// let x = 10 + 5;
+// x += 10;
+// // This x = x + 10;
+// x *= 4;
+// x++;
+// console.log(x);
 
-console.log(ageJonas > ageRyan);
-console.log(ageRyan >= 18);
+// console.log(ageJonas > ageRyan);
+// console.log(ageRyan >= 18);
 
-const isFullAge = ageRyan >= 18;
+// const isFullAge = ageRyan >= 18;
 
-console.log(currentYear - 1991 > currentYear - 2023);
+// console.log(currentYear - 1991 > currentYear - 2023);
+
+// let currentYear = 2023;
+// const ageJonas = currentYear - 1991;
+// const ageRyan = currentYear - 1995;
+// console.log(currentYear - 1991 > currentYear - 2017);
+
+// console.log(ageJonas, ageRyan);
+// const averageAge = (ageJonas + ageRyan) / 2;
+// console.log(averageAge);
+
+// CODING CHALLENGE 1
+
+// const bmi = mass / height ** 2;
+// mass in kg and height in meter
+
+// let massMark = 78;
+// let heightMark = 1.69;
+// const bmiMark = massMark / heightMark ** 2;
+// console.log(bmiMark);
+
+// let massJohn = 92;
+// let heightJohn = 1.95;
+// const bmiJohn = massJohn / heightJohn ** 2;
+// console.log(bmiJohn);
+
+// const markHigherBMI = bmiMark > bmiJohn;
+// console.log(markHigherBMI);
+
+const firstName = 'Jonas'
+const job = 'teacher';
+const birthYear = 1991;
+const year = 2023;
+
+// Old way
+const jonas = "I'm " + firstName + ", a " + (year - birthYear) + " year old " + job + "!";
+console.log(jonas);
+
+// New way
+const newJonas = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`;
+console.log(newJonas);
