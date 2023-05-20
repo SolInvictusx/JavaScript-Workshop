7a11059cca3339e6f6e4210bf20f2676bcdafd75
SolInvictusx <ambrosi52@live.com>
2023-05-20 15:35:31 -0500

    Assignment operators

--- script.js
+++ script.js
@@ -40,3 +40,41 @@ console.log(myFirstJob);
 
 // console.log(typeof null);
 
+// let age = 30;
+// age = 65;
+
+// const birthYear = 1995;
+
+// var job = 'programmer';
+// job = 'software engineer';
+
+// lastName = 'Smith';
+// console.log(lastName);
+
+let currentYear = 2023;
+const ageJonas = currentYear - 1991;
+const ageRyan = currentYear - 1995;
+console.log(ageJonas, ageRyan);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = 'Jonas';
+const lastName = 'Smith';
+
+console.log(firstName + ' ' + lastName);
+
+// Assignment operators
+let x = 10 + 5;
+x += 10;
+// This x = x + 10;
+x *= 4;
+x++;
+console.log(x);
+
+console.log(ageJonas > ageRyan);
+console.log(ageRyan >= 18);
+
+const isFullAge = ageRyan >= 18;
+
+console.log(currentYear - 1991 > currentYear - 2023);
