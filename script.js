adb0c9f6fb2020f4c63bf69a6d4b71f0d4fe11a3
SolInvictusx <ambrosi52@live.com>
2023-05-30 23:58:02 -0500

    1st

--- script.js
+++ script.js
@@ -131,3 +131,13 @@ if (age >= 18) {
     const yearsLeft = 18 - age;
     console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`);
 }
+
+const birthYear = 1995;
+let century;
+if (birthYear <= 2000) {
+    century = 20;
+} else {
+    century = 21;
+}
+
+console.log(century);
